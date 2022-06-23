e75a0b5b4ab4c78d8ad2ae7c15d95ced75301b11
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-06-23 13:27:48 -0300

    details views

--- screens/Details.js
+++ screens/Details.js
@@ -1,6 +1,82 @@
-import { Text } from 'react-native';
-const Details = () => {
-	return <Text>Details</Text>;
+import {
+	Text,
+	View,
+	SafeAreaView,
+	Image,
+	StatusBar,
+	FlatList,
+} from 'react-native';
+import React from 'react';
+import { COLORS, SHADOWS, SIZES, FONTS, assets } from '../constants';
+import {
+	CircleButton,
+	SubInfo,
+	FocusedStatusBar,
+	DetailsDesc,
+	DetailsBid,
+} from '../components';
+
+const DetailsComponent = ({ data, navigation }) => (
+	<View style={{ width: '100%', height: 370 }}>
+		<Image
+			source={data.image}
+			resizeMode='cover'
+			style={{ width: '100%', height: '100%' }}
+		/>
+		<CircleButton
+			imgUrl={assets.left}
+			handlePress={() => navigation.goBack()}
+			left={15}
+			top={StatusBar.currentHeight + 10}
+		/>
+		<CircleButton
+			imgUrl={assets.heart}
+			right={15}
+			top={StatusBar.currentHeight + 10}
+		/>
+	</View>
+);
+
+const Details = ({ route, navigation }) => {
+	const { data } = route.params;
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<FocusedStatusBar
+				barStyle='dark-content'
+				backgroundColor='transparent'
+				translucent={true}
+			/>
+			<View
+				style={{
+					width: '100%',
+					position: 'absolute',
+					bottom: '0',
+					paddingVertical: SIZES.font,
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: 'rgba(255, 255, 255,0.5)',
+					zIndex: 1,
+				}}>
+				<RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+			</View>
+			<FlatList
+				data={data.bids}
+				renderItem={({ item }) => <DetailsBid bid={item} />}
+				keyExtractor={item => item.id}
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+				ListHeaderComponent={() => (
+					<React.Fragment>
+						<DetailsComponent data={data} navigation={navigation} />
+						<SubInfo />
+						<View style={{ padding: SIZES.font }}>
+							<DetailsDesc data={data} />
+						</View>
+					</React.Fragment>
+				)}
+			/>
+		</SafeAreaView>
+	);
 };
 
 export default Details;
