9baa9698f9586456d48b0343701d0e88498242a1
Federico Pontoriero <federicolucaspontoriero@gmail.com>
2022-06-23 14:14:01 -0300

    fixed details

--- screens/Details.js
+++ screens/Details.js
@@ -1,34 +1,38 @@
+import React from 'react';
 import {
-	Text,
 	View,
+	Text,
 	SafeAreaView,
 	Image,
 	StatusBar,
 	FlatList,
 } from 'react-native';
-import React from 'react';
-import { COLORS, SHADOWS, SIZES, FONTS, assets } from '../constants';
+
+import { COLORS, SIZES, assets, SHADOWS, FONTS } from '../constants';
 import {
 	CircleButton,
+	RectButton,
 	SubInfo,
-	FocusedStatusBar,
 	DetailsDesc,
 	DetailsBid,
+	FocusedStatusBar,
 } from '../components';
 
-const DetailsComponent = ({ data, navigation }) => (
-	<View style={{ width: '100%', height: 370 }}>
+const DetailsHeader = ({ data, navigation }) => (
+	<View style={{ width: '100%', height: 373 }}>
 		<Image
 			source={data.image}
 			resizeMode='cover'
 			style={{ width: '100%', height: '100%' }}
 		/>
+
 		<CircleButton
 			imgUrl={assets.left}
 			handlePress={() => navigation.goBack()}
 			left={15}
 			top={StatusBar.currentHeight + 10}
 		/>
+
 		<CircleButton
 			imgUrl={assets.heart}
 			right={15}
@@ -39,6 +43,7 @@ const DetailsComponent = ({ data, navigation }) => (
 
 const Details = ({ route, navigation }) => {
 	const { data } = route.params;
+
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<FocusedStatusBar
@@ -46,31 +51,36 @@ const Details = ({ route, navigation }) => {
 				backgroundColor='transparent'
 				translucent={true}
 			/>
+
 			<View
 				style={{
 					width: '100%',
 					position: 'absolute',
-					bottom: '0',
+					bottom: 0,
 					paddingVertical: SIZES.font,
 					justifyContent: 'center',
 					alignItems: 'center',
-					backgroundColor: 'rgba(255, 255, 255,0.5)',
+					backgroundColor: 'rgba(255,255,255,0.5)',
 					zIndex: 1,
 				}}>
 				<RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
 			</View>
+
 			<FlatList
 				data={data.bids}
 				renderItem={({ item }) => <DetailsBid bid={item} />}
 				keyExtractor={item => item.id}
 				showsVerticalScrollIndicator={false}
-				contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+				contentContainerStyle={{
+					paddingBottom: SIZES.extraLarge * 3,
+				}}
 				ListHeaderComponent={() => (
 					<React.Fragment>
-						<DetailsComponent data={data} navigation={navigation} />
+						<DetailsHeader data={data} navigation={navigation} />
 						<SubInfo />
 						<View style={{ padding: SIZES.font }}>
 							<DetailsDesc data={data} />
+
 							{data.bids.length > 0 && (
 								<Text
 									style={{
